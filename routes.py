05f9ac6fd58b075709c93ebc670b6fced83dcb2d
errorcode-001 <76472978+VikasPoddar@users.noreply.github.com>
2021-06-25 10:45:54 +0530

    1st phase of styling and linking

--- routes.py
+++ routes.py
@@ -1,10 +1,20 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,url_for
 
 app=Flask(__name__)
 
 @app.route('/')
 def home() :
     return render_template('home.htm')
+
+@app.route('/contact')
+def contact() :
+    return render_template('contact.htm')
+
+@app.route('/about')
+def about() :
+    return render_template('about.htm')
+
+
 
 if __name__=="__main__" :
     app.run()
